ead1152e400460599b1e3f5f9c4bf4c48d26c90d
Nwogu Michael Daberechi <codesmiles@Nwogus-MacBook-Pro.local>
2024-07-23 06:50:37 +0800

    update

---
+++ tasks/promotional-task-6/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "user_michael"
+}
+
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
